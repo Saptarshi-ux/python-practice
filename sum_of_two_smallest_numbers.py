acc7a817980cb639d24d668aebf71a4b728567d2
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-03-12 11:53:49 +0530

    Update sum_of_two_smallest_numbers

--- sum_of_two_smallest_numbers.py
+++ sum_of_two_smallest_numbers.py
@@ -1,8 +1,9 @@
 def sum_two_smallest_numbers(num):
-    num.sort()
-    return num[0]+num[1]
-
-
+  if all(isinstance(x, int) and x > 0 for x in num):
+        num.sort()
+        return num[0] + num[1]
+  else:
+        return "not all the numbers are positive"
 '''
 here the challenge was: Create a function that returns the sum of the two 
 lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.

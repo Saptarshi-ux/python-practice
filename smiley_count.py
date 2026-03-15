bed8dbe8a9af8f85a5df4c5c67c094ffe5987b95
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-03-15 15:11:14 +0530

    counting the number of smileys in an array

---
+++ smiley_count.py
@@ -0,0 +1,14 @@
+# writing a code for counting the number of smileys in an array
+def count_smileys(arr):
+    count = 0
+    eyes = [':', ';']
+    noses = ['-', '~']
+    mouths = [')', 'D'] 
+    for i in arr:
+        if len(i) == 2:
+            if i[0] in eyes and i[1] in mouths:
+                count += 1
+        elif len(i) == 3:
+            if i[0] in eyes and i[1] in noses and i[2] in mouths:
+                count += 1               
+    return count

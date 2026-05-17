b26d3ef58265ff00f808e1c1fe538fde1403a2ca
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-05-17 20:16:35 +0530

    Enhance Variable References and List Reassignment examples
    Added explanations and comments for two problems demonstrating variable references and list reassignment in Python.

--- Variable_References_and_List_Reassignment.py
+++ Variable_References_and_List_Reassignment.py
@@ -1,3 +1,5 @@
+#Problem 1: 
+
 a=[1,5,7]
 b=a
 a=a+[4]
@@ -15,3 +17,18 @@ Next, a list comprehension iterates through the new list a and filters out any e
 successfully isolating the newly added number. As a result, 
 the code prints the original untouched list b ([1, 5, 7]), 
 the newly created list a ([1, 5, 7, 4]), and the extracted difference x ([4])"""
+
+#Problem 2: 
+a=[1,2]
+b=[a]
+a+=[3]
+print(b[0] is a, b)
+
+#Explanation:
+
+"""When we write a += [3], Python does not create a brand new list in memory; 
+instead, it acts just like the .extend() method and modifies the original list in-place. 
+Because b was created to hold a direct sticky note to a (making b look like [[1, 2]]),
+any in-place changes to a will instantly show up inside b. 
+Therefore, when the print statement checks if the first item in b is the exact same object as a in memory (b[0] is a),
+it correctly evaluates to True, and prints the updated contents showing that b is now [[1, 2, 3]]."""

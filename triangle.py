ccbef7748dffe4a23bc0a3e149563e168e991438
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-05-21 00:07:45 +0530

    Implement recursive triangle check function
    Added a recursive function to check if three sides can form a triangle.

--- triangle.py
+++ triangle.py
@@ -10,3 +10,17 @@ if (x1>0 and x2>0 and x3>0) and (x1+x2>x3 and x1+x3>x2 and x2+x3>x1):
   print(f'The result is:\nHere {x1},{x2} and {x3} are forming a triangle')
 else:
   print(f'The result is:\nHere {x1}, {x2} and {x3} are not forming a triangle')
+
+#Solving the same problem using a recursion function:
+
+def check_triangle(x1, x2, x3):
+    if (x1 > 0 and x2 > 0 and x3 > 0) and (x1 + x2 > x3 and x1 + x3 > x2 and x2 + x3 > x1):
+        print(f'The result is:\nHere {x1}, {x2} and {x3} are forming a triangle')
+    else:
+        print(f'The result is:\nHere {x1}, {x2} and {x3} are not forming a triangle')
+
+x1 = float(input("Enter the length of a side of a triangle: "))
+x2 = float(input("Enter the length of another side of a triangle: "))
+x3 = float(input("Enter the length of the last side of a triangle: "))
+
+check_triangle(x1, x2, x3)

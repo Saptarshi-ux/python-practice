824ceb700b00036f106078dd9c73ac5184957060
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-05-20 23:58:18 +0530

    Add triangle inequality check in triangle.py
    Implemented the Triangle Inequality Theorem to check if three lengths can form a triangle.

---
+++ triangle.py
@@ -0,0 +1,12 @@
+# implemented the Triangle Inequality Theorem to check whether three random lengths create a triangle or not
+
+x1=float(input("enter the length of a side of a triangle:"))
+x2=float(input("enter the length of another side of a triangle:"))
+x3=float(input("enter the length of last side of a triangle:"))
+
+#to check whether x1,x2 and x3 are forming any triangle or not (SSS rule)
+
+if (x1>0 and x2>0 and x3>0) and (x1+x2>x3 and x1+x3>x2 and x2+x3>x1):
+  print(f'The result is:\nHere {x1},{x2} and {x3} are forming a triangle')
+else:
+  print(f'The result is:\nHere {x1}, {x2} and {x3} are not forming a triangle')

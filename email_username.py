ccaeefd6d54d6d82f8ea355c228a8f125232d948
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-05-31 01:01:19 +0530

    Add explanation for email username extraction logic

--- email_username.py
+++ email_username.py
@@ -9,3 +9,9 @@ for i in email:
 print(f'Your email id is: {email}\nAnd Your Username is:{un}')
 
 # Explanation:
+"""if i == '@': break: This is the lookout. Before doing anything else, 
+it checks if the current letter is the @ symbol. If it is, the break command triggers. 
+break instantly kills the loop and stops checking any more letters.
+un += i: If the letter wasn't the @ symbol, the program skips the break and comes to this line.
+This takes the current letter (i) and glues it to the end of our un text box."""
+

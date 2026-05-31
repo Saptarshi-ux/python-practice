f46357e8232cc453db1fdf107e06af6f86871788
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-06-01 00:25:38 +0530

    Fix index error for empty list in largest.py
    Added a check for empty list to prevent index error.

--- largest.py
+++ largest.py
@@ -6,3 +6,8 @@ for i in x:
   if i>largest:
     largest=i
 print(f'The largest element from the list {x} is 👉 {largest}')
+
+#But in the above problem, I have found a bug which is not very serious, but it hinders the code from being perfect.
+#So I have modified that part. In the first code, if by chance the user passes an empty list, then it will throw an INDEX ERROR. So to meet this gap,
+#I have modified the code in this way:
+

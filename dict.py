5dba54d3dc3dcc4735ac538015f299d1a951bd7d
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-06-03 20:41:32 +0530

    Implement sorting of dictionary data by marks
    Add a script to sort a list of dictionaries by marks.

---
+++ dict.py
@@ -0,0 +1,14 @@
+#Sorting in a dictionary data in ascending order
+
+data=[{"ID": "A01","Name":"Rahul", "Marks": 77},
+     {"ID": "A02","Name": "Rita", "Marks": 89},
+     {"ID": "A03","Name": "Sam", "Marks": 64},
+     {"ID": "A04","Name":"Priya", "Marks": 81}]
+
+from operator import itemgetter
+sorted_data=sorted(data, key=itemgetter("Marks"))
+for r in sorted_data:
+  print(r)
+
+#Here, the sorting is based on the dictionary element "marks"
+

022f435d60b6559e1abdf3d16614e3d44ff4327f
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-06-04 23:21:20 +0530

    Implement descending sort for dictionary by 'Marks'
    Added functionality to sort dictionary data in descending order based on 'Marks'.

--- dict.py
+++ dict.py
@@ -10,5 +10,19 @@ sorted_data=sorted(data, key=itemgetter("Marks"))
 for r in sorted_data:
   print(r)
 
-#Here, the sorting is based on the dictionary element "marks"
+#Here, the sorting is based on the dictionary element "Marks"
+
+#The next part:
+
+#Now sorting in a dictionary data in descending order. The code is:
+
+data=[{"ID": "A01","Name":"Rahul", "Marks": 77},
+     {"ID": "A02","Name": "Rita", "Marks": 89},
+     {"ID": "A03","Name": "Sam", "Marks": 64},
+     {"ID": "A04","Name":"Priya", "Marks": 81}]
+
+from operator import itemgetter
+sorted_data=sorted(data, key=itemgetter("Marks"), reverse=True)     #Here just I added reverse criteria
+for r in sorted_data:
+  print(r)
 

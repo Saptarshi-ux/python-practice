795bd27912bb64d351c0c3d58a6cc73c8307b342
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-06-06 19:38:48 +0530

    Implement even/odd detection with lambda function
    Added a lambda function to detect even or odd numbers.

--- lambda/code_with_examples.py
+++ lambda/code_with_examples.py
@@ -3,3 +3,8 @@
 cube_r=lambda x: x**(1/3)
 n=int(input("enter a number:"))
 print(f'The cube root of {n} is {cube_r(n)}')
+
+#detecting even and odd
+is_even=lambda x: x%2==0
+n=int(input("enter a number:"))
+print(f'The number {n} is Even:- {is_even(n)}')

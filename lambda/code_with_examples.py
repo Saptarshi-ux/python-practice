ca89f510d2c61768eca35ffa61802bdc3a776730
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-06-07 23:27:53 +0530

    Add lambda functions for even/odd and sign checking

--- lambda/code_with_examples.py
+++ lambda/code_with_examples.py
@@ -8,3 +8,9 @@ print(f'The cube root of {n} is {cube_r(n)}')
 is_even=lambda x: x%2==0
 n=int(input("enter a number:"))
 print(f'The number {n} is Even:- {is_even(n)}')
+
+#check whether the number is positive or negative
+
+check_n=lambda x: 'Positive Number' if x>0 else ('It is Zero' if x == 0 else 'Negative Number')
+n = float(input("enter a number: "))
+print(f'the number {n} is {check_n(n)}')

28568f5b74f50db9b8b5e52459ae2ff9e4c904a1
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-06-22 19:01:32 +0530

    Add digit separation for a three-digit number

--- floor_division/example3.py
+++ floor_division/example3.py
@@ -1,1 +1,11 @@
+#Chop off the last digit
 
+
+score=int(input("enter a number in three digit"))
+first_dig=score//100
+second_dig=(score//10)%10
+third_dig=score%10
+print("\n---- After Digit Separation ----")
+print("The first digit from the left is", first_dig)
+print("The second digit from the left is", second_dig)
+print("The third digit from the left is", third_dig)

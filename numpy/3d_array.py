083b66284ca8cab3a21961b13a66e1587d1a950e
Saptarshi Bandyopadhyay <saptarshi.ban54@gmail.com>
2026-06-27 23:55:43 +0530

    Add example for 3D array in numpy/3d_array.py
    Added example usage of a 3D NumPy array with explanation.

--- numpy/3d_array.py
+++ numpy/3d_array.py
@@ -1,1 +1,8 @@
+#Example with 3D array
 
+import numpy as n
+arr=n.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr[0,1,0]+arr[1,0,2])
+
+#explanation:
+"""Here arr[block number, row number, col number]"""
